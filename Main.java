c79d2451352a650214021a8fa9d158abfb7c1aa3
Grigory <kononoff88@gmail.com>
2023-06-08 20:57:37 +0300

    +

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        LLRBT<Integer, Integer> tree = new LLRBT<>();
+        for (int i = 1; i < 15; i++) {
+            tree.add(i, i);
+        }
+        tree.delete(4);
+        System.out.println("Левостороннее красно-черное дерево:");
+        tree.print();
+    }
+}
